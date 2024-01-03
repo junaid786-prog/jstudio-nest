65c0307b279888b08e7fccae1f4362e3ce8fe78e
junaid786-prog <junaidshoaib555@gmail.com>
2024-01-03 16:54:36 +0500

    video

--- apps/processing-service/src/processing-service.controller.ts
+++ apps/processing-service/src/processing-service.controller.ts
@@ -4,12 +4,21 @@ import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class ProcessingServiceController {
-  constructor(private readonly processingServiceService: ProcessingServiceService) {}
+  constructor(private readonly processingServiceService: ProcessingServiceService) { }
 
   @MessagePattern('process_video')
   async processVideo(video: any): Promise<any> {
     try {
       let result = await this.processingServiceService.processVideo(video);
+      // this.processingServiceService.convert('./df.mp4', './output.mp3', function (err) {
+      //   if (!err) {
+      //     console.log('conversion complete');
+      //     //...
+
+      //   } else {
+      //     console.log(err);
+      //   }
+      // });
       return result;
     } catch (error) {
       throw error;
--- apps/processing-service/src/processing-service.service.ts
+++ apps/processing-service/src/processing-service.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import * as ffmpeg from 'fluent-ffmpeg'
+
 
 @Injectable()
 export class ProcessingServiceService {
@@ -12,5 +14,29 @@ export class ProcessingServiceService {
     } catch (error) {
       throw error;
     }
+  }
+
+  convert(input: string, output: string, callback: (data: any) => void) {
+    var command = ffmpeg(input).format('mp4');
+    command
+      .clone()
+      .save(output)
+      .on('end', () => {
+        console.log("stored successfully");
+
+      })
+      .catch((err: any) => {
+        console.log('Error', err);
+      });
+
+    // ffmpeg(input)
+    //   .output(output)
+    //   .on('end', function () {
+    //     console.log('conversion ended');
+    //     callback(null);
+    //   }).on('error', function (err: any) {
+    //     console.log('error: ', err);
+    //     callback(err);
+    //   }).run();
   }
 }
